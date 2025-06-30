20e04c48e9933d9517043c094fec731fd2712642
vloeiolzlr <yoonseed1995@naver.com>
2025-06-30 15:29:53 +0900

    섹션 7-1 완료

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public class MemberService {
     private final MemberRepository memberRepository;
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
     public Long join(Member member) {
         // 중복 이름 불가
         validateDuplicateMember(member);
